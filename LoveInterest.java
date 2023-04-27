cf8295f1f9c4989e839fc5382e04fe7fff6345e8
gperez42 <gperez42@smith.edu>
2023-04-26 20:19:12 -0400

    FP2 Codebase

---
+++ LoveInterest.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class LoveInterest extends Person {
+  
+    public int points; // total points
+
+  public LoveInterest(String name, String pronouns) {
+    super(name, pronouns);
+
+  }
+
+//   public givePoints() {
+//     // give points to Traveler if they choose a response with greater than 0 points
+//   }
+
+  
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\tWelcome to the Castle of Hearts, a legendary castle known for its magical power to bring true love to those who are worthy. You play as a young traveler who seeks shelter from a storm and finds yourself drawn to the castle's enchanting aura.\n" + "\tAs you enter the castle, you encounter the charming and mysterious Lord Nathaniel, the owner of the castle. Nathaniel is known for his wit, intellect, and aloof demeanor. Intrigued by his enigmatic presence, you are determined to win his heart and embark on a quest to earn his affection.\n");
+        System.out.println("**SCENE 1: Entering the Castle...**");
+        System.out.println("\tThe sky was shrouded in heavy clouds as the traveler made their way towards the Castle of Hearts. A chilly breeze swept through the air, causing the traveler to wrap their cloak tightly around their shoulders, the anticipation building in their chest.\n" + "\tAs they approached the castle gates, the rain began to drizzle, adding to the eerie atmosphere. The traveler couldn't help but feel a sense of trepidation mixed with curiosity. What secrets did the castle hold? What mysteries awaited them within its walls?\n" + "\tWith each step, the traveler's mind raced with questions. They wondered about the curse that was said to plague the castle, and about the enigmatic Nathaniel, the master of the castle, whom they were about to meet. They wondered what lay ahead, and whether they were prepared for the challenges that awaited them.\n" + "\tInside, their inner voice urged them to be cautious, to tread carefully, and to stay vigilant. They knew that Nathaniel was known for his aloof demeanor and guarded heart, and winning his trust would not be easy. But they were determined to uncover the truth.\n" + "\tAs they approached the castle entrance, the traveler's heart quickened with anticipation. They took a deep breath, steeling themselves for what lay ahead. The rain dripped from their cloak as they reached out to open the castle doors, their mind racing with curiosity, nerves, and a longing for adventure.");
+        System.out.println("...");
+        System.out.println("Nathaniel: Welcome to the Castle of Hearts, traveler. I am Lord Nathaniel. What business do you have here?");
+        
+        // if (choice == 1) {
+        //     traveler.changepoints(System.out.println("I seek shelter from the storm. Your castle seemed like the safest option.\n" + "Response: Very well. You may stay for the night, but do not overstay your welcome.")); // (+5 points)
+        // }
+    }
+}
